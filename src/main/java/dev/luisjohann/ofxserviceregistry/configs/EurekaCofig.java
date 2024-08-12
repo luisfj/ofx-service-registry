2ea6ed5245c1b452545243c8c75a24f41d33476f
Luis Johann <luisfj_pr@hotmail.com>
2024-08-12 20:49:21 +0000

    first commit

---
+++ src/main/java/dev/luisjohann/ofxserviceregistry/configs/EurekaCofig.java
@@ -0,0 +1,11 @@
+package dev.luisjohann.ofxserviceregistry.configs;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaCofig {
+
+   
+}
---
+++ src/test/java/dev/luisjohann/ofxloadbalance/OfxLoadbalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisjohann.ofxloadbalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfxLoadbalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
