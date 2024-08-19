c8a3be866699ffe3c2fe33d2fdf79e4f8c98698c
Luis Johann <luisfj_pr@hotmail.com>
2024-08-19 08:00:00 -0300

    first commit

---
+++ src/main/java/dev/luisjohann/ofxserviceregistry/OfxServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package dev.luisjohann.ofxserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfxServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfxServiceRegistryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/luisjohann/ofxserviceregistry/OfxServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisjohann.ofxserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfxLoadbalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
