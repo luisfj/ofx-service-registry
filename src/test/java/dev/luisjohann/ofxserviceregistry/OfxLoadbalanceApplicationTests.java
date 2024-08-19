2c213d64e451d09f8739e4a1c0a25f751d22c3a3
Luis Johann <luisfj_pr@hotmail.com>
2024-08-19 10:41:11 +0000

    Add lb

--- src/test/java/dev/luisjohann/ofxserviceregistry/OfxLoadbalanceApplicationTests.java
+++ src/test/java/dev/luisjohann/ofxserviceregistry/OfxLoadbalanceApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.luisjohann.ofxloadbalance;
+package dev.luisjohann.ofxserviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
